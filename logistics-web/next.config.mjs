856364431b1754f83d3309ef24f51905117ec97a
Dignified Sorinolu-Bimpe <damolasorinolu@gmail.com>
2024-06-07 03:52:01 +0100

    Added Vercel support to API III

--- logistics-web/next.config.mjs
+++ logistics-web/next.config.mjs
@@ -1,4 +1,24 @@
+import path from 'path';
+import dotenv from 'dotenv-webpack';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true, // Enable React's Strict Mode
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Add environment variables using dotenv
+    config.plugins.push(new dotenv({ path: path.join(process.cwd(), '.env.local'), systemvars: true }));
+
+    // Add custom webpack configuration
+    config.resolve.alias['@'] = path.resolve(process.cwd());
+    
+    return config;
+  },
+  // Enable usage of environment variables in the browser
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+  },
+  // Custom directory for the Next.js server
+  distDir: 'build'
+};
 
 export default nextConfig;
